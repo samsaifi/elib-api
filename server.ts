c6654ff5018647b6e912fc1c707252bc6340a112
sam saifi <saddam1234321@gmail.com>
2024-09-13 11:40:30 +0530

    setup server file

--- server.ts
+++ server.ts
@@ -1,5 +1,8 @@
-console.log("welcome to ts ");
-const same = () => {
-    console.log("welcome to inside same ");
+import app from "./src/app";
+const startServer = () => {
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log(`listening on port: ${port}`);
+    });
 };
-same();
+startServer();
