ff76f62e52bb8e59f3348dc144c5d8e244913b14
sam saifi <saddam1234321@gmail.com>
2024-09-13 11:55:09 +0530

    setup config

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,9 @@
 import express from "express";
 const app = express();
-
+//Routes
+app.get("/", (req, res, next) => {
+    res.json({
+        message: "welcome to elib apis",
+    });
+});
 export default app;
