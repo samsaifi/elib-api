0f37a2cb530377348900ab48cdab151d301dc10c
sam saifi <saddam1234321@gmail.com>
2024-09-13 11:18:58 +0530

    setup prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,14 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tsEsLint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tsEsLint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    extends: ["prettier"],
+  },
 ];
--- server.ts
+++ server.ts
@@ -1,1 +1,5 @@
 console.log("welcome to ts ");
+const same = () => {
+    console.log("welcome to inside same ");
+};
+same();
